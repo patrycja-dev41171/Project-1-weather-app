09d0ca9242d357236b1e14b3426958221325b6ca
Patrycja Ińska <patrycja.inska@interia.pl>
2022-02-04 03:35:58 +0100

    commit 1.0 layout/ Css / App - components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="first_section">
+        <input type="text" placeholder="Wyszukaj miasto"></input>
+        <h2 className="city_name">Kraków</h2>
+        <h1 className="temperature">27°C</h1>
+        <h3 className="weather_describe">Bezchmurnie</h3>
+        <div className="max_temperature">
+          <h1 className="max_temperature_element">29°C</h1>
+          <p className="max_temperature_element">Maksymalnie</p>
+        </div>
+        <div className="min_temperature">
+          <h1 className="max_temperature_element">24°C</h1>
+          <p className="max_temperature_element">Minimalnie</p>
+        </div>
+      </section>
+      <section className="second_section">
+        <div className="one"></div>
+        <div className="two"></div>
+        <div className="three"></div>
+        <div className="four"></div>
+        <div className="five"></div>
+        <div className="six"></div>
+      </section>
     </div>
   );
 }
